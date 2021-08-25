606353e2b28e7cd168b6b75ea4c5cc8e3e210bc6
Aditi Dalvi <aditidalvi22@gmail.com>
2021-08-25 15:12:01 +0530

    initial commit

---
+++ nav.js
@@ -0,0 +1,6 @@
+const toggle_button = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggle_button.addEventListener('click',()=>{
+    navbarLinks.classList.toggle('active');
+})
